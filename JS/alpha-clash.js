2047e8b28ae6fe1a9136cbfe608cb49703cb9982
contactmizan <contactmizan1206@gmail.com>
2024-02-17 02:29:16 +0600

    check correct incorrext key

--- JS/alpha-clash.js
+++ JS/alpha-clash.js
@@ -9,10 +9,52 @@
 //     playGroundSection.classList.remove('hidden');
 // }
 
+function handleKeyboardButtonPress(event) {
+    const playerPressed = event.key;
+    console.log('Player pressed:', playerPressed)
+
+    //get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    //check matched or not
+    if (playerPressed === expectedAlphabet) {
+        console.log('you got a point');
+        console.log('you have pressed correctly', expectedAlphabet);
+        //update score:
+        //1.get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        //2. increase the score by 1
+        const newScore = currentScore + 1;
+        //3. show the updated score
+        currentScoreElement.innerText = newScore;
+        //start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        console.log('you missed.you lost a life');
+        //step-1: get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        //step-2: reduce the life count
+        const newLife = currentLife - 1;
+        //step-3: display the updated life count
+        currentLifeElement.innerText = newLife;
+    }
+}
+//capture keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
 function continueGame() {
     //step-1: generate a random alphabet 
     const alphabet = getARandomAlphabet();
-    console.log('Your random alphabet', alphabet);
+    // console.log('Your random alphabet', alphabet);
 
     //set randomly generated alphabet to the screen (show it)
     const currentAlphabetElement = document.getElementById('current-alphabet');
